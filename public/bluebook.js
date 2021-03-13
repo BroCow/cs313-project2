3f4b9320c284e41f78848317673bc9f3d3a4dad2
BrowCow <cow18003@byui.edu>
2021-03-13 15:38:56 -0800

    working push

--- public/bluebook.js
+++ public/bluebook.js
@@ -24,3 +24,20 @@ function loadDoc() {
     xhttp.send();  
     //send() sends request to server (used for GET)
   }
+
+
+  function showCustomer(str) {
+    var xhttp;  
+    if (str == "") {
+      document.getElementById("txtHint").innerHTML = "";
+      return;
+    }
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "/getApprentice", true);
+    xhttp.send();
+  }
