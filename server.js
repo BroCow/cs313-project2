7207c5a28ea91845d6a6cc884b310ac87053384e
BrowCow <cow18003@byui.edu>
2021-03-12 19:22:44 -0800

    talking

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //require('dotenv').config();
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+//const PORT = process.env.PORT || 5000
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
@@ -11,11 +11,17 @@ const connectionString = process.env.DATABASE_URL || "postgres://vxpdladnpzktdo:
 const pool = new Pool({connectionString: connectionString});
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.set("port", (process.env.PORT || 5000));
+app.listen(app.get("port"), function(){
+  console.log("Now listening for connections on port: ", app.get("port"));
+})
+
+//app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   
   var sql = "SELECT * FROM apprentice";
