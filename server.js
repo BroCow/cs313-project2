ca52dee3c80a72b7b5e69a5676da64d40b069750
BrowCow <cow18003@byui.edu>
2021-03-12 17:17:33 -0800

    database

--- server.js
+++ server.js
@@ -2,9 +2,29 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const {Pool} = require('pg');
+const connectionString = process.env.DATABASE_URL || "postgres://vxpdladnpzktdo:4b561a7785a31211f719a4ce172b5108e3f5392bb21cb48152c4d069a21475b8@ec2-54-145-102-149.compute-1.amazonaws.com:5432/ddpk9b50ot9r87?ssl=true";
+const pool = new Pool({connectionString: connectionString});
+
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  var sql = "SELECT * FROM some_table_here";
+
+  pool.query(sql, function(err, result) {
+      // If an error occurred...
+      if (err) {
+          console.log("Error in query: ")
+          console.log(err);
+      }
+  
+      // Log this to the console for debugging purposes.
+      console.log("Back from DB with result:");
+      console.log(result.rows);
+  
+  
+  });     
